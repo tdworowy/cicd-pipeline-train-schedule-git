4fc2c936060e031a9f96048360f1b75ac6217fa7
Tomasz <dworowytomasz@gmail.com>
2019-04-26 13:32:29 +0200

    set longer timeouts in tests

--- test/index.test.js
+++ test/index.test.js
@@ -5,12 +5,14 @@ var app = require('../app');
  
 describe('Index Page', function() {
   it("renders successfully", function(done) {
+    this.timeout(1000);
     request(app).get('/').expect(200, done);    
   })
 })
 
 describe('Trains API', function() {
   it("returns data successfully", function(done) {
+    this.timeout(1000);
     request(app).get('/trains').expect(200).expect('Content-Length', '1093').expect('Content-Type', /json/, done); ;    
   })
 })
